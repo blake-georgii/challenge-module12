c5a1068420362bf98f21711eb94ef8242bb34c15
Blake Georgii <blakegeorgii@gmail.com>
2022-01-31 19:57:35 -0800

    set up main question branch skelton

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
 
-const originalQuestion = [
+const startQuestion = [
     {
         type: 'list',
-        name: 'originalChoice',
+        name: 'startChoice',
         message: 'Day:',
-        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
     }
 ];
 
+function handleStartQuest({ startChoice }) {
 
+    switch (startChoice) {
+        case 'View all departments':
+            break;
+
+        case 'View all roles':
+            break;
+
+        case 'View all employees':
+            break;
+
+        case 'Add a department':
+            break;
+
+        case 'Add a role':
+            break;
+
+        case 'Add an employee':
+            break;
+
+        case 'Update an employee role':
+            break;
+
+        case 'Quit':
+            console.log('Thanks for using the Deparment Mangager!');
+            break;
+    }
+}
 
 
 
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
-    inquirer.prompt(originalQuestion)
-        .then(answer => { console.log(answer)})
+    inquirer.prompt(startQuestion)
+        .then(answer => { handleStartQuest(answer) })
         .catch(err => { console.log(err) })
 });
