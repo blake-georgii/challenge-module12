182d349c342ef2da513e9ed48358f0047e4dc637
Blake Georgii <blakegeorgii@gmail.com>
2022-01-31 20:24:55 -0800

    added some spacing to make things easier to read

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ function viewAllDepos() {
             console.log(err);
             return;
         }
+        console.log("\n");
         console.table(rows);
     });
     handleStartQuest();
@@ -68,6 +69,7 @@ function viewAllRoles() {
             console.log(err);
             return;
         }
+        console.log("\n");
         console.table(rows);
     });
     handleStartQuest();
@@ -80,6 +82,7 @@ function viewAllEmployees() {
             console.log(err);
             return;
         }
+        console.log("\n");
         console.table(rows);
     });
     handleStartQuest();
